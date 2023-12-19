18536a842c9d3c8c5afcee51e0ba57016a4addbd
martsb <marts.bicevskis@va.lv>
2023-12-19 10:13:10 +0000

    a

---
+++ migrations/20210925135800-add-ast-country-to-ast-table.sql
@@ -0,0 +1,1 @@
+ALTER TABLE ast_daily ADD COLUMN country varchar(10) DEFAULT 'USA';
--- naked.py
+++ naked.py
@@ -43,10 +43,12 @@ except:
     logger.exception('')
 logger.info('DONE')
 
+# Initialize database
 def init_db():
         global connection
         connection = mysql.connector.connect(host=mysql_config_mysql_host, database=mysql_config_mysql_db, user=mysql_config_mysql_user, password=mysql_config_mysql_pass)
 
+# Trying to make a connection with database
 def get_cursor():
         global connection
         try:
@@ -84,7 +86,7 @@ def mysql_insert_ast_into_db(create_date, hazardous, name, url, diam_min, diam_m
                 logger.error( "INSERT INTO `ast_daily` (`create_date`, `hazardous`, `name`, `url`, `diam_min`, `diam_max`, `ts`, `dt_utc`, `dt_local`, `speed`, `distance`, `ast_id`) VALUES ('" + str(create_date) + "', '" + str(hazardous) + "', '" + str(name) + "', '" + str(url) + "', '" + str(diam_min) + "', '" + str(diam_max) + "', '" + str(ts) + "', '" + str(dt_utc) + "', '" + str(dt_local) + "', '" + str(speed) + "', '" + str(distance) + "', '" + str(ast_id) + "')")
                 logger.error('Problem inserting asteroid values into DB: ' + str(e))
                 pass
-
+# Checking and adding asteroid into DB, if it is not there already
 def push_asteroids_arrays_to_db(request_day, ast_array, hazardous):
         for asteroid in ast_array:
                 if mysql_check_if_ast_exists_in_db(request_day, asteroid[9]) == 0:
@@ -93,6 +95,7 @@ def push_asteroids_arrays_to_db(request_day, ast_array, hazardous):
                 else:
                         logger.debug("Asteroid already IN DB")
 
+# Defining where main loop starts
 if __name__ == "__main__":
 
     connection = None
@@ -130,17 +133,21 @@ if __name__ == "__main__":
     logger.debug("Response headers: " + str(r.headers))
     logger.debug("Response content: " + str(r.text))
 
+    # Checking if connection was succesful
     if r.status_code == 200:
 
+            # Parsing a response as a json structure
             json_data = json.loads(r.text)
 
+            # Initializing arrays for safe and hazardous asteroids
             ast_safe = []
             ast_hazardous = []
 
+            # Checking if matches with the one in json data
             if 'element_count' in json_data:
                     ast_count = int(json_data['element_count'])
                     logger.info("Asteroid count today: " + str(ast_count))
-
+                    # If there are asteriods, getting their data and writing them into the database
                     if ast_count > 0:
                             for val in json_data['near_earth_objects'][request_date]:
                                     if 'name' and 'nasa_jpl_url' and 'estimated_diameter' and 'is_potentially_hazardous_asteroid' and 'close_approach_data' in val:
@@ -148,6 +155,7 @@ if __name__ == "__main__":
                                             tmp_ast_nasa_jpl_url = val['nasa_jpl_url']
 					    # Getting id of asteroid
                                             tmp_ast_id = val['id']
+                                            # Getting estimated diameter for asteroids and if it is harzardous
                                             if 'kilometers' in val['estimated_diameter']:
                                                     if 'estimated_diameter_min' and 'estimated_diameter_max' in val['estimated_diameter']['kilometers']:
                                                             tmp_ast_diam_min = round(val['estimated_diameter']['kilometers']['estimated_diameter_min'], 3)
@@ -161,17 +169,20 @@ if __name__ == "__main__":
 
                                             tmp_ast_hazardous = val['is_potentially_hazardous_asteroid']
 
+                                            # Getting asteroid stats and checking if they are close to Earth
+
                                             if len(val['close_approach_data']) > 0:
                                                     if 'epoch_date_close_approach' and 'relative_velocity' and 'miss_distance' in val['close_approach_data'][0]:
                                                             tmp_ast_close_appr_ts = int(val['close_approach_data'][0]['epoch_date_close_approach']/1000)
                                                             tmp_ast_close_appr_dt_utc = datetime.utcfromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
                                                             tmp_ast_close_appr_dt = datetime.fromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
 
+                                                            # Checking if asteroids velocity is known
                                                             if 'kilometers_per_hour' in val['close_approach_data'][0]['relative_velocity']:
                                                                     tmp_ast_speed = int(float(val['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']))
                                                             else:
                                                                     tmp_ast_speed = -1
-
+                                                            # Checking if it is known, how close to Earth the asteroid will get
                                                             if 'kilometers' in val['close_approach_data'][0]['miss_distance']:
                                                                     tmp_ast_miss_dist = round(float(val['close_approach_data'][0]['miss_distance']['kilometers']), 3)
                                                             else:
@@ -188,6 +199,7 @@ if __name__ == "__main__":
                                                     tmp_ast_speed = -1
                                                     tmp_ast_miss_dist = -1
 
+                                            # Printing out info about the asteroid
                                             logger.info("------------------------------------------------------- >>")
                                             logger.info("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
                                             logger.info("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
@@ -199,11 +211,13 @@ if __name__ == "__main__":
                                             else:
                                                     ast_safe.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist, tmp_ast_id])
 
+                    # If there were no asteroids found today printing that out
                     else:
                             logger.info("No asteroids are going to hit earth today")
 
             logger.info("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
 
+            # Sorting hazardous asteroids, if there are some
             if len(ast_hazardous) > 0:
 
                     ast_hazardous.sort(key = lambda x: x[4], reverse=False)
@@ -220,6 +234,7 @@ if __name__ == "__main__":
                     if len(ast_safe) > 0:
                         push_asteroids_arrays_to_db(request_date, ast_safe, 0)
 
+    # Printing error message, if connection failed
     else:
             logger.error("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
 
